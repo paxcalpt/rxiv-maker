f0953be60d0fdff8c12b9daf49d6d63fce22f9e6
Ricardo Henriques <paxcalpt@gmail.com>
2025-06-20 22:10:31 +0100

    Setup versioneer for automatic version management

--- src/py/__init__.py
+++ src/py/__init__.py
@@ -3,5 +3,13 @@
 A comprehensive toolkit for automated scientific article generation and building.
 """
 
-__version__ = "1.0.0"
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
+
 __author__ = "RXiv-Maker Contributors"
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
